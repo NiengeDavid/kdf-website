34d16d26fa2d6686f9a8a22f2d7e952bd36d92dc
David Nienge <davidnienge@gmail.com>
2025-04-18 06:29:38 +0100

    ui fixes

--- components/home/redbanner.tsx
+++ components/home/redbanner.tsx
@@ -19,19 +19,21 @@ export default function Banner({ data }: { data: BannerProps["data"] }) {
         <h2 className="text-3xl font-bold mb-6 md:mb-10">{data?.title}</h2>
 
         {/* Description */}
-        <p className="text-lg mb-10 font-normal md:mb-14 md:font-semibold md:text-xl">{data?.desc}</p>
+        <p className="text-lg mb-10 font-normal md:mb-14 md:font-semibold md:text-xl">
+          {data?.desc}
+        </p>
 
         {/* Buttons */}
         <div className="flex justify-center gap-4">
-        <Link
+          <Link
             href={data.buttons[0].link}
-            className="bg-white text-black text-lg font-medium border hover:bg-secondary-red hover:text-white border-white px-5 md:px-8 py-2 rounded-sm"
+            className="bg-white text-black text-sm font-medium border hover:bg-secondary-red hover:text-white border-white px-2 md:px-8 py-2 rounded-sm"
           >
             {data.buttons[0].text}
           </Link>
-        <Link
+          <Link
             href={data.buttons[1].link}
-            className="bg-secondary-red text-white text-lg font-medium border hover:bg-white hover:text-black hover:border-transparent border-white px-5 md:px-8 py-2 rounded-sm"
+            className="bg-secondary-red text-white text-sm font-medium border hover:bg-white hover:text-black hover:border-transparent border-white px-2 md:px-8 py-2 rounded-sm"
           >
             {data.buttons[1].text}
           </Link>
